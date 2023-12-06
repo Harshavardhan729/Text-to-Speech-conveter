4d060925bc72ef8a386a88a13d0585bc3fdf0798
Harshavardhan Thammisetti <140039372+Harshavardhan729@users.noreply.github.com>
2023-12-06 21:16:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const speakBtn = document.querySelector("#playBtn");
+const query = document.querySelector("#text");
+speakBtn.addEventListener("click", playSpeech);
+function playSpeech() {
+    const speech = new SpeechSynthesisUtterance(query.value);
+    speechSynthesis.speak(speech);
+}
